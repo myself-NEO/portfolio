dbb6d2efc19bf3aa3405f0f7377b39d1dbb71f7b
Keshav Kumar Jha <40928377+myself-NEO@users.noreply.github.com>
2024-10-07 02:59:45 +0530

    Homepage created

---
+++ src/components/Common/notFound.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NotFound = () => {
+  return (
+    <div style={{display: 'flex', alignItems: 'center', justifyContent: 'center', color: 'red'}}>
+      <h1>404 - Page Not Found</h1>
+      
+    </div>
+  );
+};
+
+export default NotFound;
